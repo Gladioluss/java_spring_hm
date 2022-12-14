8667092c9e591b4d362dbb39cf91b5d65210527e
Danil <74546538+Gladioluss@users.noreply.github.com>
2022-12-14 15:56:19 +0500

    Add files via upload

---
+++ buns/homeWork8/IntelliJIDEAConfiguration/src/main/java/com/example/homework8idea/HomeWork8IdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoework8idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork8IdeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork8IdeaApplication.class, args);
+	}
+
+}
---
+++ buns/homeWork8/IntelliJIDEAConfiguration/src/test/java/com/example/homework8idea/HomeWork8IdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework8idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork8IdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
