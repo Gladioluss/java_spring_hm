64dbaac617de58af4d474ae6249461106f602c54
Danil <74546538+Gladioluss@users.noreply.github.com>
2022-12-14 15:58:02 +0500

    Add files via upload

---
+++ buns/homeWork8/SpringInitializrCinfiguration/src/main/java/com/example/homeWork8Web/HomeWork8WebApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeWork8Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork8WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork8WebApplication.class, args);
+	}
+
+}
---
+++ buns/homeWork8/SpringInitializrCinfiguration/src/test/java/com/example/homeWork8Web/HomeWork8WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeWork8Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork8WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
